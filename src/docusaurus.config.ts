078459a434d99d1edde1d9f38e693a036a8086a6
Gabriel Palma <gubpalma@hotmail.com>
2026-03-01 16:41:26 +1100

    config: updated site URL and base URL for deployment adjustments

--- src/docusaurus.config.ts
+++ src/docusaurus.config.ts
@@ -16,10 +16,10 @@ const config: Config = {
     },
 
     // Set the production url of your site here
-    url: 'https://gman-au.github.io',
+    url: 'https://bottle-rocket.app',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/bottle-rocket-documentation/',
+    baseUrl: '/',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
